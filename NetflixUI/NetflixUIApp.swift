eabece614d6c435b2c6a0b6815c834414c993303
Christian Manzaraz <magnusmanz@icloud.com>
2025-03-26 12:47:37 -0300

    Initial Commit

---
+++ NetflixUI/NetflixUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixUIApp.swift
+//  NetflixUI
+//
+//  Created by Christian Manzaraz on 26/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct NetflixUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
